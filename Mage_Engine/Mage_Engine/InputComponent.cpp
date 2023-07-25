3abac15cf544c866005281a6202f33ed72b6d205
MartinJ95 <martinjones95@gmx.co.uk>
2023-07-25 14:42:57 +0100

    feat editorCam : full input implementation
    this commit should:
    
    implement classes for mouse move result plus key results for press/release.
    allow the editor cam to listen for the keys and notify the resulting code to execute on notify.
    hide cursor on right click
    show cursor on right click release
    rotate editor cam on mouse move as long as right click is currently on the lock state.

--- Mage_Engine/Mage_Engine/InputComponent.cpp
+++ Mage_Engine/Mage_Engine/InputComponent.cpp
@@ -10,8 +10,8 @@ void MouseMoveInput::Notify(const Mage::Maths::Vector3& vector)
 	if (m_lastMousePos == vector)
 		return;
 
+	InputSubject::Notify(vector - m_lastMousePos);
 	m_lastMousePos = vector;
-	InputSubject::Notify(vector);
 }
 
 ButtonInput::ButtonInput(int buttonID) : m_buttonID(buttonID)
@@ -23,12 +23,35 @@ const int ButtonInput::GetButton() const
 	return m_buttonID;
 }
 
+InputComponent::InputComponent(Entity& entity)
+	: Component(entity), InputObserver(), m_mouseMoveDetection(), m_mouseMoveResult(), m_cursorLock(),
+	m_mouseButtonInputs(),m_keyboardButtonInputs(), m_axisInputs(),
+	m_MovementVector(0.f, 0.f, 0.f), m_locked(false)
+{
+}
+
+InputComponent::~InputComponent()
+{
+	/*for (auto i : m_mouseButtonInputs)
+	{
+		delete i;
+	}
+	for (auto i : m_keyboardButtonInputs)
+	{
+		delete i;
+	}*/
+}
+
 void InputComponent::Update(Application& app)
 {
 	double x, y;
 	GLFWwindow* window = app.m_viz->getWindow();
+
+	if (window == nullptr)
+		return;
+
 	glfwGetCursorPos(window, &x, &y);
-	m_mouseMoveDetection.Notify(Mage::Maths::Vector3(x, y, 0));
+	m_mouseMoveDetection.Notify(Mage::Maths::Vector3(y, x, 0));
 
 	auto checkButtons = [](std::vector<ButtonInput>& buttons, GLFWwindow* w, int(*getInput)(GLFWwindow*, int))
 	{
@@ -42,13 +65,22 @@ void InputComponent::Update(Application& app)
 				break;
 			case GLFW_RELEASE:
 				buttons[i].Notify(false);
+				break;
 			default:
+				break;
 			}
 		}
 	};
 
 	checkButtons(m_mouseButtonInputs, window, glfwGetMouseButton);
 	checkButtons(m_keyboardButtonInputs, window, glfwGetKey);
+
+	Transform* t = m_entity.getComponent<Transform>();
+
+	if (t == nullptr)
+		return;
+
+	t->m_position += t->m_forward * m_MovementVector.z + t->m_forward.Cross(app.m_worldUp) * m_MovementVector.z + app.m_worldUp * m_MovementVector.y;
 
 	/*
 	auto checkButton = [](ButtonInput& i, GLFWwindow* w, const int button, int(*getInput)(GLFWwindow*, int))
@@ -109,3 +141,86 @@ void InputComponent::Update(Application& app)
 	*/
 }
 
+void InputComponent::Initialize(Application& app)
+{
+	m_mouseMoveResult.AssignApplication(app, *this);
+	m_cursorLock.AssignApplication(app, *this);
+
+	m_mouseButtonInputs.emplace_back(GLFW_MOUSE_BUTTON_RIGHT);
+	m_mouseButtonInputs[0].Subscribe(&m_cursorLock);
+	m_mouseMoveDetection.Subscribe(&m_mouseMoveResult);
+
+	std::pair<int, Mage::Maths::Vector3> axisKeys[]{
+		{GLFW_KEY_W, Mage::Maths::Vector3(0.f, 0.f, 1.f)},
+		{GLFW_KEY_S, Mage::Maths::Vector3(0.f, 0.f, -1.f)},
+		{GLFW_KEY_D, Mage::Maths::Vector3(1.f, 0.f, 0.f)},
+		{GLFW_KEY_A, Mage::Maths::Vector3(-1.f, 0.f, 0.f)},
+	};
+	for (int i = 0; i < 4; i++)
+	{
+		m_keyboardButtonInputs.emplace_back(axisKeys[i].first);
+		m_axisInputs.emplace_back(app, *this, axisKeys[i].second);
+		m_keyboardButtonInputs[i].Subscribe(&m_axisInputs[i]);
+	}
+}
+
+InputResult::InputResult() : m_app(nullptr), m_owner(nullptr)
+{
+
+}
+
+InputResult::InputResult(InputComponent& owner) : m_app(nullptr), m_owner(&owner)
+{
+
+}
+
+InputResult::InputResult(Application& app, InputComponent& owner) : m_app(&app), m_owner(&owner)
+{
+
+}
+
+AxisInput::AxisInput(Application& app, InputComponent& owner, Mage::Maths::Vector3& axis) : InputResult(app, owner), m_axis(axis)
+{
+
+}
+
+void AxisInput::OnNotify(bool Pressed)
+{
+	if (m_app == nullptr || m_owner == nullptr)
+		return;
+
+	m_owner->m_MovementVector += Pressed ? m_axis : -m_axis;
+}
+
+void InputResult::AssignApplication(Application& app)
+{
+	m_app = &app;
+}
+
+void InputResult::AssignApplication(Application& app, InputComponent& owner)
+{
+	m_app = &app;
+	m_owner = &owner;
+}
+
+void MouseMoveInputRotation::OnNotify(Mage::Maths::Vector3& vector)
+{
+	if (m_owner == nullptr)
+		return;
+
+	Transform* t = m_owner->m_entity.getComponent<Transform>();
+	
+	if (t == nullptr || m_app == nullptr)
+		return;
+
+	t->m_rotation += vector;
+	t->updateDirection();
+}
+
+void EditorCamLockCursorResult::OnNotify(bool Pressed)
+{
+	if (m_app == nullptr || m_owner == nullptr)
+		return;
+
+	glfwSetInputMode(m_app->m_viz->getWindow(), GLFW_CURSOR, Pressed ? GLFW_CURSOR_HIDDEN : GLFW_CURSOR_NORMAL);
+}
--- Mage_Engine/Mage_Engine/InputComponent.h
+++ Mage_Engine/Mage_Engine/InputComponent.h
@@ -2,7 +2,7 @@
 #include "Core/Component.h"
 #include "Observer.h"
 
-class MouseMoveInput : InputSubject
+class MouseMoveInput : public InputSubject
 {
 public:
     MouseMoveInput();
@@ -20,14 +20,61 @@ private:
     int m_buttonID;
 };
 
+class InputComponent;
+
+class InputResult : public InputObserver
+{
+public:
+    InputResult();
+    InputResult(InputComponent& owner);
+    InputResult(Application& app, InputComponent& owner);
+    void AssignApplication(Application& app);
+    void AssignApplication(Application& app, InputComponent& owner);
+protected:
+    Application* m_app;
+    InputComponent* m_owner;
+};
+
+class MouseMoveInputRotation : public InputResult
+{
+public:
+    virtual void OnNotify(Mage::Maths::Vector3 &vector) override;
+};
+
+class EditorCamLockCursorResult : public InputResult
+{
+public:
+    virtual void OnNotify(bool Pressed) override;
+};
+
+class AxisInput : public InputResult
+{
+public:
+    AxisInput(Application& app, InputComponent& owner, Mage::Maths::Vector3 &axis);
+    virtual void OnNotify(bool Pressed) override;
+private:
+    Mage::Maths::Vector3 m_axis;
+};
+
+//todo
+//split up editor cam specific to derived class (when it all works)
 class InputComponent :
     public Component, public InputObserver
 {
 public:
+    friend class AxisInput;
+    InputComponent(Entity& entity);
     virtual void Update(Application& app) override;
-private:
+    virtual void Initialize(Application& app);
+    ~InputComponent();
+protected:
     MouseMoveInput m_mouseMoveDetection;
+    MouseMoveInputRotation m_mouseMoveResult;
+    EditorCamLockCursorResult m_cursorLock;
     std::vector<ButtonInput> m_mouseButtonInputs;
     std::vector<ButtonInput> m_keyboardButtonInputs;
+    std::vector<AxisInput>  m_axisInputs;
+    Mage::Maths::Vector3 m_MovementVector;
+    bool m_locked;
 };
 
